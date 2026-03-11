833055c598e76f7a618c161169ae984c38df9bcc
John West <john@Johns-MacBook-Pro.local>
2026-03-11 16:31:45 -0400

    do not use TS in prod

--- scripts/render-start.sh
+++ scripts/render-start.sh
@@ -7,4 +7,4 @@ if [ -n "${GOOGLE_SERVICE_ACCOUNT_JSON:-}" ]; then
   printf "%s" "$GOOGLE_SERVICE_ACCOUNT_JSON" > /var/data/google-credentials.json
 fi
 
-exec node --loader ts-node/esm src/app.ts
+exec node dist/app.js
